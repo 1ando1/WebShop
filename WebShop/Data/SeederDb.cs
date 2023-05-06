bf75c221cbb4825a72dfa7f08009f9e1e7dd3d6b
1ando11 <you@example.com>
2023-05-06 12:27:29 +0200

    Finish CRUD. Add Identity

--- WebShop/Data/SeederDb.cs
+++ WebShop/Data/SeederDb.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+using WebShop.Constants;
 using WebShop.Data;
+using WebShop.Data.Entities.Identity;
 
 public static class SeederDB
 {
@@ -10,6 +14,41 @@ public static class SeederDB
         {
             var context = scope.ServiceProvider.GetRequiredService<AppEFContext>();
             context.Database.Migrate();
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+
+            if (!context.Roles.Any())
+            {
+                RoleEntity admin = new RoleEntity()
+                {
+                    Name = Roles.Admin
+                };
+
+                RoleEntity user = new RoleEntity()
+                {
+                    Name = Roles.User
+                };
+                var result = roleManager.CreateAsync(admin).Result;
+                result = roleManager.CreateAsync(user).Result;
+            }
+
+            if (!context.Users.Any())
+            {
+                UserEntity user = new UserEntity()
+                {
+                    FirstName = "Anton",
+                    LastName = "Dobrovolskyi",
+                    Email = "dobrovolskuy.anton2006@gmail.com",
+                    UserName = "dobrovolskuy.anton2006@gmail.com"
+                };
+
+                var result = userManager.CreateAsync(user).Result;
+                if (result.Succeeded)
+                {
+                    result = userManager.AddToRoleAsync(user, Roles.Admin).Result;
+                }
+            }
         }
     }
 }
---
+++ WebShop/Models/AccountViewModels.cs
@@ -0,0 +1,10 @@
+﻿public class RegisterUserViewModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string ImageBase64 { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+
+}
